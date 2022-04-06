a33d2a89f9eda24ab58f3f6bd4f70391c8ad9971
ArodDaBoss <38160409+ArodDaBoss@users.noreply.github.com>
2022-04-06 17:28:02 -0400

    Getting information

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 from auth import get_auth
 import requests
+import random
 
 def get_data():
     access_token = get_auth()
@@ -7,10 +8,29 @@ def get_data():
         'Authorization': 'Bearer {TOKEN}'.format(TOKEN=access_token)
 
     }
-    URL =	'https://api.spotify.com/v1/artists/{id}/top-tracks'.format(id='2hlmm7s2ICUX0LVIhVFlZQ')
+    URL =	'https://api.spotify.com/v1/artists/{id}/top-tracks'.format(id='3hcs9uc56yIGFCSy9leWe7')
     data = requests.get(URL + "?market=US", headers= headers)
     data = data.json()
+
+    #rand=random.randint(0,len(data['tracks'])-1)
+    rand = 2
     print(data)
+
+    x =data["tracks"][rand]["external_urls"]["spotify"]
+    song_name=data["tracks"][rand]["name"]
+    artist_names = []
+
+    for i in data["tracks"][rand]["name"]:
+        artist_names.append(i["name"])
+    
+    artist_names = data["tracks"][rand]["name"]
+    album_image = data["tracks"][rand]["album"]["images"][0]["url"]
+    song_preview = data["tracks"][rand]["preview_url"] #can be null
+    spotify_link = data["tracks"][rand]["external_urls"]["spotify"]
+
+    info = [song_name,artist_names,album_image,song_preview,spotify_link]
+
+    return info
 
 if __name__ == '__main__':
     get_data()
